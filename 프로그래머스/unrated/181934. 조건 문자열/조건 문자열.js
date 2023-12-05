0904f5e909a5d17a349558b874437459e0639b0a
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-12-05 14:45:45 +0900

    [level unrated] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,16 @@
+function solution(ineq, eq, n, m) {
+    let answer = 0;
+    if(ineq === "<" ) {
+        if (eq === "="){
+            answer = n <= m ? 1 : 0;
+        }else {
+            answer = n < m ? 1 : 0;
+        }
+    }else {
+        if (eq === "=") {
+            answer = n >= m ? 1 : 0;
+        }else {
+            answer = n > m ? 1 : 0;
+        }
+    }return answer;
+}
