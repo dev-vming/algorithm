a28a05ef5604aeabb8bad66b0a53b3c2ffb733ce
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-12-10 02:10:39 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 20.51 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    var answer = [];
+    queries.map(([s,e,k])=>{
+        let temp = arr.filter((v, i)=>(i>=s && i<=e && v>k)).sort((a,b)=>a-b)[0];
+        answer.push(temp? temp : -1);
+    })
+    return answer;
+}
