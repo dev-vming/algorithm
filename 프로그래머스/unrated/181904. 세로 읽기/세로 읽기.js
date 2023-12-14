aa60e675bf11eb805c049551eabc78adb68240a4
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-12-14 17:53:22 +0900

    [level unrated] Title: 세로 읽기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,8 +1,7 @@
 function solution(my_string, m, c) {
     var answer = '';
     for (let i = 0; i <my_string.length; i+=m){
-        let cutted_str = my_string.substr(i,m);
-        answer += cutted_str[c-1];
+        answer += my_string[i+c-1];
         
     }
     return answer;
