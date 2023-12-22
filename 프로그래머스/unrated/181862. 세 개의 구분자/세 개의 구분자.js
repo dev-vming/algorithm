91ae4855990a576fe079568651ca20caaa3428c6
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-12-23 00:49:45 +0900

    [level unrated] Title: 세 개의 구분자, Time: 31.31 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -0,0 +1,4 @@
+function solution(myStr) {
+    const arr = myStr.replaceAll('a',' ').replaceAll('b',' ').replaceAll('c',' ').split(" ").filter((v)=>v!=="");
+    return arr.length === 0 ? ["EMPTY"] : arr;
+}
