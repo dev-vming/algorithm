0cd500fd546bdeb866f883de881a6c326bb732f3
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-12-26 01:24:01 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,21 @@
+function solution(arr1, arr2) {
+    let sum1 =0;
+    let sum2 =0;
+    if(arr1.length === arr2.length){
+        for(let i =0; i<arr1.length; i++){
+            sum1 += arr1[i];
+            sum2 += arr2[i];
+        }
+        if(sum1 === sum2){
+                return 0;
+            }else if (sum1 > sum2){
+                return 1;
+            }else {
+                return -1;
+            }
+    } else if(arr1.length > arr2.length){
+        return 1;
+    } else {
+        return -1;
+    }
+}
