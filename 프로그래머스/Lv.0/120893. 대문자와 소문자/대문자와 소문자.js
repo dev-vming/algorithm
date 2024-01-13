cba281fe287f3df42c72aeb1531120d419ee3da9
dev-vming <142391557+dev-vming@users.noreply.github.com>
2024-01-13 22:46:25 +0900

    [level Lv.0] Title: 대문자와 소문자, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+    return [...my_string].map((s)=>s===s.toUpperCase()
+                              ? s.toLowerCase()
+                             : s.toUpperCase())
+    .join("");
+}
