3425486f0191fd5c8c0a7b8241c19bfb4b2fa213
dev-vming <142391557+dev-vming@users.noreply.github.com>
2024-01-19 22:33:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let answer = [];
+    for(let i = 0 ; i<numbers.length; i++){
+        for(let j=0; j<numbers.length; j++){
+            i!==j && answer.push(numbers[i]*numbers[j])
+        }  
+    }
+    return Math.max(...answer);
+}
