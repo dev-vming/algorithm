605a1ce16a15d318d2b68cac99a6e430d9c88b06
dev-vming <142391557+dev-vming@users.noreply.github.com>
2024-02-16 21:16:27 +0900

    [Bronze V] Title: A+B - 3, Time: 196 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze V/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const num = parseInt(input[0]);
+
+for(let i =1; i<=num; i++){
+    let score = input[i].split(' ');
+    console.log(parseInt(score[0])+parseInt(score[1]));
+}
