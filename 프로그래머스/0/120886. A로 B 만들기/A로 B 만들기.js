02395f7f613e8c3d16017e6ed7c3e0d63d0b3f7e
dev-vming <142391557+dev-vming@users.noreply.github.com>
2025-01-05 20:41:51 +0900

    [level 0] Title: A로 B 만들기, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,9 @@
+function solution(before, after) {
+    
+    const beforeSorted = before.split('').sort().join('');
+    const afterSorted = after.split('').sort().join('');
+    
+    
+    return beforeSorted === afterSorted ? 1 : 0;
+
+}
