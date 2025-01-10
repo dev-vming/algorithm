e40c135577b9201a82a1765f6905409632a5f480
dev-vming <142391557+dev-vming@users.noreply.github.com>
2025-01-10 17:58:58 +0900

    [Silver V] Title: 수들의 합, Time: 168 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.js
+++ 백준/Silver/1789. 수들의 합/수들의 합.js
@@ -3,10 +3,9 @@ const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 let sum = 0
 let cnt = 0;
 
-while (sum < input) {
+while (sum <= input) {
   cnt++;
   sum += cnt;
-  if (sum > input) cnt--;
 }
 
-console.log(cnt);
+console.log(cnt-1);
