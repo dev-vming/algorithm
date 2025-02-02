77e77b7a14a95d8ec13dcef0db66b151d31e6adf
dev-vming <142391557+dev-vming@users.noreply.github.com>
2025-02-03 08:36:15 +0900

    [Silver II] Title: 예산, Time: 168 ms, Memory: 11936 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.js
@@ -0,0 +1,25 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const M = Number(input[2]);
+
+let start = 1;
+let end = Math.max(...arr);
+let result = 0;
+
+while (start <= end) {
+  let mid = parseInt((start + end) / 2); 
+  let total = 0;
+  for (x of arr) {
+    total += Math.min(mid, x);
+  }
+  if (total <= M) {
+    result = mid;
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+
+console.log(result);
