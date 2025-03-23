2063dbb3f999ae23dbb41cd298b288738164e38b
MINJEONG YANG <142391557+dev-vming@users.noreply.github.com>
2025-03-23 22:46:27 +0900

    [Bronze V] Title: A+B - 8, Time: 104 ms, Memory: 9540 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const T = Number(input.shift());
+
+for (let i = 0; i < T; i++){
+  const [A, B] = input[i].split(" ").map(Number);
+  console.log(`Case #${i + 1}: ${A} + ${B} = ${A + B}`);
+}
