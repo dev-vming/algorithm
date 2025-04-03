c83aa48b6a0ea9e77f36857a965b7f90667fc556
MINJEONG YANG <142391557+dev-vming@users.noreply.github.com>
2025-04-03 17:21:03 +0900

    [Bronze II] Title: 커트라인, Time: 92 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+const [N, k] = input[0].split(' ').map(Number);
+const scores = input[1].split(' ').map(Number);
+
+console.log(scores.sort((a, b) => b - a)[k - 1]);
